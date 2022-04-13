e3dee8796a13f5f7ed87cf9252394b1a933e6a82
Emile Tenezakis <e.tenezakis@gmail.com>
2022-04-13 22:05:44 +0900

    have everything i need for the map shortest path search method impl to be written

--- src/main/MapGraph.java
+++ src/main/MapGraph.java
@@ -1,8 +1,22 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+
+import java.util.Comparator;
+
+import org.yaml.snakeyaml.Yaml;
+import java.io.InputStream;
+import java.io.File;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
 
 /*
  * a kind of ConcreteGraph, with numerical labels representing distances between points in space. 
@@ -11,39 +25,81 @@ import java.util.Set;
 public class MapGraph {
 
 	// a particular type of ConcreteGraph
-	public ConcreteGraph<String, Double> weightedGraph;
+	public ConcreteGraph<String, Double> concreteGraphMap;
 	
 	/*
+	 * TODO not sure how this handles empty entries in the yaml (cities not connected to any other city
 	 * @param filePath absolute path to yaml file representing this graph
 	 */
 	public MapGraph(String filePath) {
-		this.weightedGraph = new ConcreteGraph<String, Double>();
-		// file with yaml info 
-		// first add cities
-		// then add nodes! 
+		
+		Yaml yaml = new Yaml();
+		this.concreteGraphMap = new ConcreteGraph<String, Double>();		
+		try {
+			InputStream inputStream = new FileInputStream(new File(filePath));		
+			Map<String, Map<String, Double>> obj = yaml.load(inputStream);
+			for (Map.Entry<String, Map<String, Double>> node : obj.entrySet()) {	
+				if (!this.concreteGraphMap.listNodes().contains(node.getKey())) {
+					this.concreteGraphMap.addNode(node.getKey());									
+				}
+				for (Map.Entry<String, Double> path: node.getValue().entrySet()) {	
+					this.concreteGraphMap.addEdge(path.getKey(), node.getKey(), path.getValue());
+				}	
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found ! " + filePath);
+		}		
+		
+
 	}
 	
-	/*
-	 * Find shortest path from A to B using Dijkstra's algorithm. 
-	 */
-	public MapPath shortestPath(String startPoint, String endPoint) {
-		return new MapPath();
-	}
-	
+//	/*
+//	 * Find shortest path from A to B using Dijkstra's algorithm. 
+//	 * If there are several solutions, doesn't make any promise about which one it will pick. 
+//	 */
+//	public MapPath shortestPath(String startPoint, String endPoint) {
+//		
+//		String x = new String(startPoint);
+//		String y = new String(endPoint);
+//		Set<String> visitedSet = new LinkedHashSet<String>();		
+//		PriorityQueue<MapPath> pathQueue = new PriorityQueue<MapPath>(100, new MapPathComparator());
+//		
+//		// fill the HashMap with the key/val for the start node, with 0 as distance
+//		// fill the rest with infinite vlaues 
+//		/*
+//		 * Pseudocode 
+//		 * 
+//		 * 
+//		 */
+//		return new MapPath();
+//	}
+//	
 	
 	/*
 	 * represents an edge between two spatial points in the graph, and the distance associated with it. 
 	 */
-	public class MapEdge{
+	public static class MapEdge{
 		
-		public String start;
-		public String end;
-		public Double distance;
+		private final String start;
+		private final String end;
+		private final Double distance;
 		
 		public MapEdge(String start, String end, Double distance) {
 			this.start = start;
 			this.end = end;
 			this.distance = distance;
+		}
+		
+		public String getStart() {
+			return new String(this.start);
+		}
+		
+		public String getEnd() {
+			return new String(this.end);
+		}
+		
+		public Double getDistance() {
+			return Double.valueOf(this.distance);
 		}
 		
 	}
@@ -52,16 +108,16 @@ public class MapGraph {
 	 * represents a set of edges starting from A and ending in B, in a map represented as a graph. 
 	 * It's a path in the sense that it's continuous (one can go through it and arrive at B). 
 	 */
-	public class MapPath {
+	public static class MapPath {
 		
-		private Set<MapEdge> edges;
+		private LinkedHashSet<MapEdge> edges;		
 		private Double totalDistance = 0.;
 		
 		/*
 		 * empty set of MapEdge
 		 */
 		public MapPath() {
-			this.edges = new HashSet<MapEdge>();
+			this.edges = new LinkedHashSet<MapEdge>();
 		}
 		
 		/*
@@ -69,26 +125,26 @@ public class MapGraph {
 		 */
 		public void addEdge(MapEdge edge){
 			this.edges.add(edge);
-			this.totalDistance = this.totalDistance + edge.distance;
+			this.totalDistance = this.totalDistance + edge.getDistance();
 		}
 		
 		/*
-		 * @return the starting point
+		 * @return the starting point. Based on insertion order of edges. 
 		 */
 		public String startNode() {
 			if (!this.edges.isEmpty()) {
-				return new ArrayList<MapEdge>(this.edges).get(0).start;
+				return new ArrayList<MapEdge>(this.edges).get(0).getStart();
 			} else {
 				throw new RuntimeException("empty path");
 			}
 		}
 
 		/*
-		 * @return the end point. 
+		 * @return the end point. Based on insertion order of edges. 
 		 */
 		public String endNode() {
 			if (!this.edges.isEmpty()) {
-				return new ArrayList<MapEdge>(this.edges).get(this.edges.size()-1).start;
+				return new ArrayList<MapEdge>(this.edges).get(this.edges.size()-1).getEnd();
 			} else {
 				throw new RuntimeException("empty path");
 			}
@@ -106,4 +162,15 @@ public class MapGraph {
 		}
 		
 	}
+	
+	/*
+	 * Two MapPaths are compared based on their total distances
+	 */
+	public static class MapPathComparator implements Comparator<MapPath>{
+	     public int compare(MapPath path1, MapPath path2) {	    	 	
+	         return path1.totalDistance().compareTo(path2.totalDistance());
+	     }
+	 }
+	
+	
 }
--- src/tests/MapGraphTest.java
+++ src/tests/MapGraphTest.java
@@ -1,32 +1,84 @@
-//package tests;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import main.MapGraph;
-//
-//class MapGraphTest {
-//
-//	/*
-//	 * Going to build a very simple MapGraph representing four cities, i
-//	 * 
-//	 */
-//	MapGraph thisGraph;
-//	
-//	@BeforeAll
-//	void setUpBeforeClass() throws Exception {
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.MapGraph;
+import main.MapGraph.MapPath;
+
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+
+@TestInstance(Lifecycle.PER_CLASS)
+class MapGraphTest {
+
+	public MapGraph thisGraph;
+	public String yamlFileString = "/Users/emile/Documents/shiny-graph/src/tests/testMapGraph.yaml";
+	public String exampleEdgeStartString = "start";
+	public String exampleEdgeEndString = "end";
+	public Double exampleEdgeDistanceDouble = 10.;
+	
+	@BeforeAll
+	void setUpBeforeClass() throws Exception {
+		thisGraph = new MapGraph("/Users/emile/Documents/shiny-graph/src/tests/testMapGraph.yaml");
+	}
+
+	
+	@Test
+	void testConstructor() {
+		
+		assertTrue(thisGraph.concreteGraphMap.listNodes().contains("Tetrov"));
+		assertTrue(thisGraph.concreteGraphMap.listNodes().contains("Wishka"));
+		assertTrue(thisGraph.concreteGraphMap.listChildren("Tetrov").containsKey("Ziouxuan"));
+		assertTrue(!thisGraph.concreteGraphMap.listChildren("Tetrov").containsKey("Wishka"));
+		assertTrue(thisGraph.concreteGraphMap.listChildren("Tetrov").get("Larti").contains(50.));
+
+	}
+
+	@Test
+	void testMapEdge() {
+		MapGraph.MapEdge thisEdge = new MapGraph.MapEdge(exampleEdgeStartString, exampleEdgeEndString, exampleEdgeDistanceDouble);
+		assertTrue(thisEdge.getStart().equals(exampleEdgeStartString));
+		assertTrue(thisEdge.getEnd().equals(exampleEdgeEndString));
+		assertTrue(thisEdge.getDistance().equals(exampleEdgeDistanceDouble));
+	}
+	
+	@Test
+	void testMapPath() {
+		MapGraph.MapPath thisPath = new MapGraph.MapPath();
+		thisPath.addEdge(new MapGraph.MapEdge(exampleEdgeStartString, exampleEdgeEndString, exampleEdgeDistanceDouble));
+		assertTrue(thisPath.totalDistance().equals(exampleEdgeDistanceDouble));
+		assertTrue(thisPath.startNode().equals(exampleEdgeStartString));
+		assertTrue(thisPath.endNode().equals(exampleEdgeEndString));
+		thisPath.addEdge(new MapGraph.MapEdge("other node start", "other node end", 20.));
+		assertTrue(thisPath.totalDistance().equals(exampleEdgeDistanceDouble + 20.));
+		assertTrue(thisPath.startNode().equals(exampleEdgeStartString));
+		assertTrue(thisPath.endNode().equals("other node end"));
+	}
+	
+	@Test 
+	void testMapPathComparator() {
+		MapGraph.MapPath thisPath = new MapGraph.MapPath();
+		thisPath.addEdge(new MapGraph.MapEdge(exampleEdgeStartString, exampleEdgeEndString, exampleEdgeDistanceDouble));
+		MapGraph.MapPath otherPath = new MapGraph.MapPath();
+		otherPath.addEdge(new MapGraph.MapEdge("other node start", "other node end", 20.));
+		MapGraph.MapPathComparator mapPathComparator = new MapGraph.MapPathComparator();
+		assertTrue(mapPathComparator.compare(otherPath, thisPath)>0);
+	}
+	
+//	@Test 
+//	void testShortestPath() {
+//		
+//		/*
+//		 * Expects : 
+//		 * Wishka - Marman - Panju - Etrios - Larti, cost of 40
+//		 * lowest cost path between Wishka and Larti
+//		 */
 //	}
-//
-//	@BeforeEach
-//	void setUp() throws Exception {
-//	}
-//
-//	@Test
-//	void test() {
-//		fail("Not yet implemented");
-//	}
-//
-//}
+	// For shortest, might want to draw a graph....
+
+}
